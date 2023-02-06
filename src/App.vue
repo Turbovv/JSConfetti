c1ed064d2b5961a052f0bfbbcdeb2a5f19a16883
Giga Elizbarashvili <giga.elizbarashvili13@gmail.com>
2023-02-06 19:41:14 +0400

    Added new files

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1 @click="showConfetti">🎉 Press me! {{counter}}</h1>
+</template>
+
+<script>
+import JSConfetti from 'js-confetti'
+
+const confetti = new JSConfetti()
+
+export default {
+  data() {
+    return {
+      counter:-1
+    }
+  },
+  mounted() { 
+    this.showConfetti()
+  },
+  methods: {
+    showConfetti() {
+      confetti.addConfetti()
+      this.counter++
+    }
+  },
+}
+</script>
+
+<style>
+h1 {
+  text-align: center;
+  cursor: pointer;
+  margin-top: 3em;
+}
+</style>
